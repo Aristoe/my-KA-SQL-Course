e69522c1528a916b8a33592a9f71107a0efa2821
Aristoe <redtoblack356@gmail.com>
2017-04-26 12:45:05 -0600

    Update .sql

--- .sql
+++ .sql
@@ -1,11 +1,8 @@
-# my-KA-SQL-Course
-KhanAcademy
-
 /**creating todo list to get ready to leave**/
 
 CREATE TABLE todo (id INTEGER PRIMARY KEY, name TEXT, time_in_minutes INTEGER);
 
-INSERT INTO todo VALUES (1, "Fix Motorcycle", 120);
+INSERT INTO todo VALUES (1, "Fix Motorcycle", 120);           /**task + number of minutes to complete**/
 INSERT INTO todo VALUES (2, "Get things together", 5*60);
 INSERT INTO todo VALUES (3, "Learn Lines", 120*5);
 INSERT INTO todo VALUES (4, "MOS Certification", 6*60);

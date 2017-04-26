7206ba73a22d9e6956781b1b9b88681bdea9bcae
Aristoe <redtoblack356@gmail.com>
2017-04-26 12:36:18 -0600

    Create .SQL

---
+++ .SQL
@@ -0,0 +1,14 @@
+# my-KA-SQL-Course
+KhanAcademy
+
+/**creating todo list to get ready to leave**/
+
+CREATE TABLE todo (id INTEGER PRIMARY KEY, name TEXT, time_in_minutes INTEGER);
+
+INSERT INTO todo VALUES (1, "Fix Motorcycle", 120);
+INSERT INTO todo VALUES (2, "Get things together", 5*60);
+INSERT INTO todo VALUES (3, "Learn Lines", 120*5);
+INSERT INTO todo VALUES (4, "MOS Certification", 6*60);
+
+SELECT * FROM todo ORDER BY time_in_minutes;
+SELECT SUM(time_in_minutes/60) FROM todo;
